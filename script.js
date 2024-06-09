c6eca6a9e9660a125f5802698f7b14bdc513c02b
phornnato <natophorn@gmail.com>
2024-06-09 11:24:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(){
+    window.func2 = function() {
+        var change = document.getElementById("demo");
+        if (change) {
+            change.innerHTML = "<p>i love you</p><img id='pic' src='https://s3.getstickerpack.com/storage/uploads/sticker-pack/milk-n-mocha-1/sticker_7.gif?3b0113a9484ddf2a389e6bdf5c69c2fa'/>";
+        }
+    };
+    window.func3 = function() {
+        var change = document.getElementById("demo");
+        if (change) {
+            change.innerHTML = "<p>i hate you</p><img id='pic' src='https://s3.getstickerpack.com/storage/uploads/sticker-pack/milk-n-mocha-1/sticker_9.gif?3b0113a9484ddf2a389e6bdf5c69c2fa'/>";
+        }
+    };
+    window.func4 = function() {
+        var change = document.getElementById("demo");
+        if (change) {
+            change.innerHTML = "<p>will you married me, hik hik </p><img id='pic' src='https://media.tenor.com/y_nhpANxpPYAAAAi/kiss-cute.gif'/>";
+        }
+    };
+});
